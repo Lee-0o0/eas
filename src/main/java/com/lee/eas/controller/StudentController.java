61f1eff380e62a2ae2a58ce254ec52b672a60d7d
水木子 <773702776@qq.com>
2022-06-27 22:43:29 +0800

    add web front

--- src/main/java/com/lee/eas/controller/StudentController.java
+++ src/main/java/com/lee/eas/controller/StudentController.java
@@ -6,18 +6,49 @@ import com.lee.eas.domain.dto.StudentDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Controller
 public class StudentController {
 
+    @ResponseBody
     @GetMapping("/student")
     public Response<Pagination<StudentDTO>> queryStudents(@RequestParam int page,
                                                           @RequestParam int pageSize){
         if(page <= 0){
             page = 1;
         }
+        if(pageSize <= 0){
+            pageSize = 10;
+        }
 
-        return null;
+        Response<Pagination<StudentDTO>> response = new Response<>();
+
+
+        response.setCode(200);
+        response.setMsg("");
+        Pagination<StudentDTO> pagination = new Pagination<>();
+        pagination.setPage(page);
+        pagination.setPageSize(pageSize);
+        pagination.setTotal(new Random().nextInt() + 100);
+
+        List<StudentDTO> studentDTOList = new ArrayList<>() ;
+        for(int i = 0; i < pageSize; i++){
+            studentDTOList.add(new StudentDTO(i,"姓名" + i,"学号"+i));
+        }
+        pagination.setData(studentDTOList);
+
+        response.setData(pagination);
+        return response;
+    }
+
+    @GetMapping("/admin/index")
+    public String toIndex(){
+        return "/admin/index";
     }
 
 }
--- src/main/java/com/lee/eas/domain/dto/StudentDTO.java
+++ src/main/java/com/lee/eas/domain/dto/StudentDTO.java
@@ -1,4 +1,16 @@
 package com.lee.eas.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentDTO {
+    private int id;
+
+    private String name;
+
+    private String studentNumber;
 }
